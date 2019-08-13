41a41f93431ca54385b4211d8e2c1f7ff037cb8b
tomoya yoshida <ihatov08@gmail.com>
2019-08-13 23:12:21 +0900

    Setup for use activestorage

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,7 @@
 class Project < ApplicationRecord
   belongs_to :category
+
+  has_one_attached :main_image
+
+  has_many_attached :sub_images
 end
--- app/models/project_plan.rb
+++ app/models/project_plan.rb
@@ -1,2 +1,3 @@
 class ProjectPlan < ApplicationRecord
+  has_many_attached :images
 end
