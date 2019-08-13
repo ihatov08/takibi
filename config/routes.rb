c400d998414844750605616189e7a62c506c9afa
tomoya yoshida <ihatov08@gmail.com>
2019-08-13 15:38:35 +0900

    Implement facebook omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  root to: "home#index"
+  get 'home/index'
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'users/registrations'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
